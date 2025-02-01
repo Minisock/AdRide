a5b7ec00a141772eeff94aa21fa83e1f4b332d8c
Kundan Kumar <kun9577@gmail.com>
2025-02-01 16:10:03 +0530

    URL Fixed

--- project/src/app.js
+++ project/src/app.js
@@ -30,7 +30,7 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     secure: process.env.NODE_ENV === 'production',
-    maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+    maxAge: 7*24 * 60 * 60 * 1000 // 7 days
   }
 }));
 
